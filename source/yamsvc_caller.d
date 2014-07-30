c0c41eb53061c29dc33af79a703b3c74c8086fc2
Wai Yi Leung <w.y.leung@e-sensei.nl>
2014-07-30 16:51:11 +0200

    Refactor, clean code and set output dir for bins

--- source/yamsvc_caller.d
+++ source/yamsvc_caller.d
@@ -17,6 +17,8 @@ import std.stdio;
 import std.parallelism;
 import std.algorithm : count, max, reduce;
 import std.math : abs;
+import std.csv;
+import std.file;
 
 import bio.bam.referenceinfo;
 
@@ -27,7 +29,7 @@ import bio.bam.writer;
 
 import std.parallelism;
 
-import yamsvc.utils: is_concordant, has_minimum_quality;
+import yamsvc.utils;
 
 void printUsage() {
     stderr.writeln("Usage: yamsvp-caller [options] <input.bam | input.sam>");
@@ -40,10 +42,52 @@ void printUsage() {
     stderr.writeln("                    Turn on verbose mode");
 }
 
+class CallRegion {
+    BedRecord bed;
+    
+    uint[] mate_chromosome;
+    ulong[] mate_positions;
+    uint[] insertsizes;
+    uint clusterID;
+    ushort[] orientations;
+    uint start;
+    uint end;
+    
+    uint[][] adj_pos;
+    
+    /* not necessary needed, only when this is re-evaluated for the 2nd time */
+    void adjust_pos ( uint s_pos, uint e_pos ) {
+        this.adj_pos ~= [s_pos, e_pos]; 
+    }
+    
+    void addOrientation( short orientation ) {
+        this.orientations ~= orientation;
+    }
+    
+    void add_matechromosome( uint matechr ) {
+        this.mate_chromosome ~= matechr;
+    }
+    void add_mate_pos( ulong matepos ) {
+        this.mate_positions ~= matepos;
+    }
+
+    void addInsertsize( uint insertsize ) {
+        this.insertsizes ~= abs(insertsize);
+    }
+
+    @property uint avgInsertsize(){
+        auto sum = reduce!((a,b) => a + b)(0, this.insertsizes);
+        if( sum == 0 ) {
+            return 0;
+        }
+        return cast(uint) abs(sum / this.insertsizes.length);
+    }
+}
+
 int main(string[] args) {
-	int n_threads = 4;
-	bool verbose;
-	string bed_file;
+    int n_threads = 4;
+    bool verbose;
+    string bed_file;
 
     getopt(
         args,
@@ -61,26 +105,75 @@ int main(string[] args) {
     auto task_pool = new TaskPool(n_threads);
     scope(exit) task_pool.finish();
     
-    auto bam = new BamReader(args[1], task_pool);
     string bamfile = args[1];
-//    auto dst = new BamWriter(args[2], 9); 	// maximal compression
+    auto bam = new BamReader(bamfile, task_pool);
+    
+    // open a bed file
+    auto bedrecords = readText(bed_file);
+    auto records = csvReader!BedRecord(bedrecords, null , '\t');
+    foreach(record; records){
+        // select the discordant reads from this region
+        
+        CallRegion cr = new CallRegion();
+        cr.bed = record;
+        
+        auto reads = bam[ record.chromosome ][cast(uint) record.start .. cast(uint) record.end];
+        foreach( read; reads ) {
+            if ( is_concordant(read) ){
+                /* We don't want to analyse concordant reads, only discordant are used for SV calling */
+                continue;
+            }
+            
+            if ( !has_minimum_quality(read, 20)) {
+                continue;
+            }
+            
+            writefln("%s\t%d\t%s\t%d\tbin:%s\ttlen:%d\tflag:%d\tMQ:%d\tBaseCovered:%d", 
+                read.ref_id, 
+                read.position, 
+                read.mate_ref_id, 
+                read.mate_position, 
+                read.bin, 
+                read.template_length, 
+                read.flag,
+                read.mapping_quality, 
+                read.basesCovered() );
+
+
+            cr.adjust_pos( read.position, read.position+read.basesCovered() );
+            
+            cr.add_matechromosome( read.mate_ref_id );
+            cr.add_mate_pos( read.mate_position );
+            cr.addInsertsize( read.template_length );
+        }
+        writefln("%s\t%d\t%d\t%s\t%d",
+            record.chromosome,
+            record.start,
+            record.end,
+            record.infofields,
+            cr.avgInsertsize
+            );
+    }
+    
+    
+//    auto dst = new BamWriter(args[2], 9);     // maximal compression
 //    scope (exit) dst.finish();              // close the stream at exit
 //    dst.writeSamHeader(bam.header);         // copy header and reference sequence info
 //    dst.writeReferenceSequenceInfo(bam.reference_sequences);
     
     /*
-    	read region definitions,
-    	sort the regions by chromosome/contig, start
-    		- filter by given DPdis
-    		- filter out any clen < readlength, we are not capable calling these
-    	chunk regions by: (n regions / n_treads) * 50?
-    		- start analysis of each region in a new task
-    		- tasks reports to RegionCollection class (shared ipc)
-    	match regions
-    	annotate regions (DEL, INS, INV, ITX, CTC etc.)
-    	write vcf file
+        read region definitions,
+        sort the regions by chromosome/contig, start
+            - filter by given DPdis
+            - filter out any clen < readlength, we are not capable calling these
+        chunk regions by: (n regions / n_treads) * 50?
+            - start analysis of each region in a new task
+            - tasks reports to RegionCollection class (shared ipc)
+        match regions
+        annotate regions (DEL, INS, INV, ITX, CTC etc.)
+        write vcf file
     */
     
-	return 0;
+    return 0;
 }
 
--- source/yamsvc_regionbed.d
+++ source/yamsvc_regionbed.d
@@ -28,153 +28,80 @@ import bio.bam.pileup;
 import bio.bam.read;
 import bio.bam.writer;
 
-import yamsvc.utils: is_concordant, has_minimum_quality;
-
-class DiscordantRegion {
-	ulong start = 0;
-	ulong end = 0;
-	short chromosome=-1;
-	ushort[] coverages_discordant;
-	ushort[] coverages;
-	bool working;
-	
-	void addCoverage( ushort cov ) {
-		this.coverages ~= cov;
-	}
-	
-	void addCoverageDiscordant( ushort cov ) {
-		this.coverages_discordant ~= cov;
-	}
-	
-	ushort maxCoverage(){
-		if(this.coverages.length){
-			return reduce!(max)(this.coverages);
-		} else {
-			return 0;
-		}
-	}
-	
-	ushort maxCoverageDiscordant(){
-		if(this.coverages_discordant.length){
-			return reduce!(max)(this.coverages_discordant);
-		} else {
-			return 0;
-		}
-	}
-	
-	ushort avgCoverage(){
-		/* FIXME: cast from ulong to int is not so performing? 
-			We are only interested in coverages upto ushort (65,535) large?
-		*/
-		auto sum = reduce!((a,b) => a + b)(0, this.coverages);
-		if( sum == 0 ) {
-			return 0;
-		}
-		return cast(ushort) abs(sum / this.coverages.length);
-	}
-	
-	void resetCoverage() {
-		/*
-			Empty the coverages for a new region run
-		*/
-		this.coverages = [];
-		this.coverages_discordant = [];
-	}
-	
-	ulong length() {
-		return this.end - this.start;
-		
-	}
-	
-	void startReporting( ushort ref_id, ulong start, ulong end ) {
-		this.working = true;
-		this.chromosome = ref_id;
-		this.start = start;
-		this.end = end;
-		this.resetCoverage();
-	}
-	void startReporting( ) {
-		this.working = true;
-	}
-}
-
-
-uint n_threads;
+import yamsvc.utils;
 
 void printBedRegion(string reference_name, ref DiscordantRegion bed ) {
-	if ( bed.length() && bed.maxCoverageDiscordant() ) {
-		writefln("%s\t%d\t%d\tDP=%d;DPdis=%d;CLEN=%d", 
-			reference_name, 
-			bed.start, 
-			bed.end+1, 
-			bed.avgCoverage(), 
-			bed.maxCoverageDiscordant(), 
-			bed.length()
-		);
-	}
+    if ( bed.length() && bed.maxCoverageDiscordant() ) {
+        writefln("%s\t%d\t%d\tDP=%d;DPdis=%d;CLEN=%d", 
+            reference_name, 
+            bed.start, 
+            bed.end+1, 
+            bed.avgCoverage(), 
+            bed.maxCoverageDiscordant(), 
+            bed.length()
+        );
+    }
 }
 
 void makeRegion(BamReader bam, ReferenceSequenceInfo reference_sequence, ushort cutoff_min, ushort cutoff_max, ushort window_width) {
-	auto reads = bam[ cast(string) reference_sequence.name() ][1 .. reference_sequence.length];
-	auto pileup = makePileup(reads, true);
+    auto reads = bam[ cast(string) reference_sequence.name() ][1 .. reference_sequence.length];
+    auto pileup = makePileup(reads, true);
 
-	int lastChrom = -1;
-	ulong lastPos = 0;
-	ulong lastPosStart = 0;
-		
-	DiscordantRegion bed = new DiscordantRegion();
-		
-	foreach (column; pileup) {
-		auto coverage = 0;
-		auto discordant_coverage = 0;
-		
-		foreach (read; column.reads) {
-			if( is_concordant(read) && has_minimum_quality(read, 20) ) {
-				coverage++;
-			}
-			if (!is_concordant(read) && has_minimum_quality(read, 20) ) {
-				discordant_coverage++;
-			}
-		}
+    int lastChrom = -1;
+        
+    DiscordantRegion bed = new DiscordantRegion();
+        
+    foreach (column; pileup) {
+        auto coverage = 0;
+        auto discordant_coverage = 0;
+        
+        foreach (read; column.reads) {
+            if( is_concordant(read) && has_minimum_quality(read, 20) ) {
+                coverage++;
+            }
+            if (!is_concordant(read) && has_minimum_quality(read, 20) ) {
+                discordant_coverage++;
+            }
+        }
 
-		// should be dynamic range
-		if ( discordant_coverage >= cutoff_min && 
-			discordant_coverage <= cutoff_max ) {
-			// check whether to output or not
-			// quick patch for initializing the lastChrom
-			if ( lastChrom == -1 ) {
-				lastChrom = column.ref_id;
-				bed.chromosome = cast(short) column.ref_id;
-			}
+        // should be dynamic range
+        if ( discordant_coverage >= cutoff_min && 
+            discordant_coverage <= cutoff_max ) {
+            // check whether to output or not
+            // quick patch for initializing the lastChrom
+            if ( lastChrom == -1 ) {
+                lastChrom = column.ref_id;
+                bed.chromosome = cast(short) column.ref_id;
+            }
 
-			// We are working on the same chromosome as before
-			bool same_chrom = bed.chromosome == column.ref_id;
-			bool in_window = (abs(column.position - window_width) < bed.end) && (bed.end < column.position);
-			
-			if ( same_chrom && in_window ) {
-				bed.end = column.position;
-				bed.addCoverage( cast(ushort) coverage );
-				bed.addCoverageDiscordant( cast(ushort) discordant_coverage );
-			}
-			else {
-				if ( bed.working ) {
-					// report only if we were working on a bed track.
-					// and if we have logical results
-					printBedRegion( bam.reference_sequences[bed.chromosome].name, bed );
-				}
+            // We are working on the same chromosome as before
+            bool same_chrom = bed.chromosome == column.ref_id;
+            bool in_window = (abs(column.position - window_width) < bed.end) && (bed.end < column.position);
+            
+            if ( same_chrom && in_window ) {
+                bed.end = column.position;
+                bed.addCoverage( cast(ushort) coverage );
+                bed.addCoverageDiscordant( cast(ushort) discordant_coverage );
+            }
+            else {
+                if ( bed.working ) {
+                    // report only if we were working on a bed track.
+                    // and if we have logical results
+                    printBedRegion( bam.reference_sequences[bed.chromosome].name, bed );
+                }
 
-				// start new bed track for the new region
-				bed.startReporting( cast(ushort) column.ref_id, column.position, column.position );
-			}
-		}
-	}
+                // start new bed track for the new region
+                bed.startReporting( cast(ushort) column.ref_id, column.position, column.position );
+            }
+        }
+    }
 }
 
 void makeRegion(string bamfile, ReferenceSequenceInfo reference_sequence, ushort cutoff_min, ushort cutoff_max, ushort window_width) {
     auto tp = new TaskPool(2);
     scope(exit) tp.finish();
-	auto bam = new BamReader(bamfile, tp);
-	makeRegion(bam, reference_sequence, cutoff_min, cutoff_max, window_width);
+    auto bam = new BamReader(bamfile, tp);
+    makeRegion(bam, reference_sequence, cutoff_min, cutoff_max, window_width);
 }
 
 void printUsage() {
@@ -194,20 +121,20 @@ void printUsage() {
 
 int main(string[] args) {
 
-	int n_threads = std.parallelism.totalCPUs;
-	bool verbose;
-	ushort window_width = 200;
-	ushort cutoff_max = 90;
-	ushort cutoff_min = 4;
+    int n_threads = std.parallelism.totalCPUs;
+    ushort window_width = 200;
+    ushort cutoff_max = 90;
+    ushort cutoff_min = 4;
+    bool verbose;
 
     getopt(
         args,
         std.getopt.config.caseSensitive,
         "threads|T",  &n_threads,    // numeric
-        "window|w", &window_width,
-        "cutoff_max|h", &cutoff_max,
-        "cutoff_min|l", &cutoff_min,
-        "verbose|v", &verbose,   // flag
+        "window|w", &window_width,   // numeric
+        "cutoff_max|h", &cutoff_max, // numeric
+        "cutoff_min|l", &cutoff_min, // numeric
+        "verbose|v", &verbose,       // flag
         );
 
     if (args.length < 2) {
@@ -215,17 +142,23 @@ int main(string[] args) {
         return 0;
     }
     
+    // free some threads for the bamreader, which also uses a Taskpool for reading operations11
+    n_threads = n_threads > 5 ? n_threads - 2 : n_threads;
+    // this a dangerous operation, what if we still overuse the amount of 'threads' on the host sytem?
+
+
     auto task_pool = new TaskPool(n_threads);
     scope(exit) task_pool.finish();
     
-	string bamfile = args[1];
-	auto bam = new BamReader(bamfile);
+    string bamfile = args[1];
+    auto bam = new BamReader(bamfile);
+
     
     foreach (refseq; bam.reference_sequences) {
-//    	writefln("Starting thread for %s", refseq);
-    	auto t = task!makeRegion(bamfile, refseq, cutoff_min, cutoff_max, window_width);
-    	task_pool.put(t);
-    	}
+//      writefln("Starting thread for %s", refseq);
+        auto t = task!makeRegion(bamfile, refseq, cutoff_min, cutoff_max, window_width);
+        task_pool.put(t);
+        }
 
-	return 0;
+    return 0;
 }
